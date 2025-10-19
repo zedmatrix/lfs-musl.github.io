fb81cd2674443355ec7c655af140abba9cae26b5
zedmatrix <zedmatrix007@gmail.com>
2025-10-19 10:31:57 -0700

    musl yaml definitions

---
+++ repos/gcc-libs/sources/testsuite-gcc-15.2.0.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Starting - ${PKGNAME} - ${PKGVER} - Testsuite"
+
+echo 'int main(){}' | cc -x c - -v -Wl,--verbose &> dummy.log
+readelf -l a.out | grep ': /lib'
+grep -E -o '/usr/lib.*/S?crt[1in].*succeeded' dummy.log
+grep -B4 '^ /usr/include' dummy.log
+grep 'SEARCH.*/usr/lib' dummy.log |sed 's|; |\n|g'
+grep "/lib.*/libc.so.6 " dummy.log
+grep found dummy.log
+rm -v a.out dummy.log
+mkdir -pv /usr/share/gdb/auto-load/usr/lib
+mv -v /usr/lib/*gdb.py /usr/share/gdb/auto-load/usr/lib
+
+echo "Finished - ${PKGNAME} - ${PKGVER} - Testsuite"
